055f57fa2e38af4acc1d5e842be92a92d70f6bbd
Maycon Robert Brolacci <maycon.brolacci@hotmail.com>
2019-05-12 00:53:24 -0300

    Background Image Working

--- Jogos de Cartas/Main.c
+++ Jogos de Cartas/Main.c
@@ -1,110 +1,61 @@
-#include <stdio.h>
+// Inclui o Allegro 5
 #include <allegro5/allegro.h>
+// Inclui o cabeçalho do add-on para uso de imagens
+#include <allegro5/allegro_image.h>
+//Inclui  as mensagens Allegro
+#include <allegro5/allegro_native_dialog.h>
 
-const float FPS = 60;
-const int SCREEN_W = 640;
-const int SCREEN_H = 480;
-const int BOUNCER_SIZE = 32;
 
-int main(int argc, char **argv) {
-	ALLEGRO_DISPLAY *display = NULL;
-	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-	ALLEGRO_TIMER *timer = NULL;
-	ALLEGRO_BITMAP *bouncer = NULL;
-	float bouncer_x = SCREEN_W / 2.0 - BOUNCER_SIZE / 2.0;
-	float bouncer_y = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;
-	float bouncer_dx = -4.0, bouncer_dy = 4.0;
-	bool redraw = true;
+// Função Main
+int main(void)
+{
+	// Variável representando a janela principal
+	ALLEGRO_DISPLAY *janela = NULL;
 
+	// Variável representando a imagem
+	ALLEGRO_BITMAP *imagem = NULL;
+
+	// Inicializa a biblioteca do Allegro
+	al_init();
+	
+	//Tenta inicializar o Allegro
 	if (!al_init()) {
-		fprintf(stderr, "failed to initialize allegro!\n");
-		return -1;
+		al_show_native_message_box(janela, "Error", "Erro", "Falha ao inicializar o Allegro!",
+			NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return 0;
 	}
 
-	timer = al_create_timer(1.0 / FPS);
-	if (!timer) {
-		fprintf(stderr, "failed to create timer!\n");
-		return -1;
+	// Inicializa o add-on para utilização de imagens
+	al_init_image_addon();
+
+	// Cria a janela. Largura X Altura
+	janela = al_create_display(1024, 768);
+
+	// Carrega a imagem de fundo
+	imagem = al_load_bitmap("../background.jpg");
+
+	//Checa se a imagem carregou corretamente
+	if (!imagem) {
+		al_show_native_message_box(janela, "Error", "Erro", "Falaha ao carregar a imagem",
+			NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		al_destroy_display(janela);
+		return 0;
 	}
 
-	display = al_create_display(SCREEN_W, SCREEN_H);
-	if (!display) {
-		fprintf(stderr, "failed to create display!\n");
-		al_destroy_timer(timer);
-		return -1;
-	}
+	// Desenha a imagem na tela. Os valores representam a distância das bordas da imagem na tela
+	al_draw_bitmap(imagem, 0, 0, 0);
 
-	bouncer = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
-	if (!bouncer) {
-		fprintf(stderr, "failed to create bouncer bitmap!\n");
-		al_destroy_display(display);
-		al_destroy_timer(timer);
-		return -1;
-	}
-
-	al_set_target_bitmap(bouncer);
-
-	al_clear_to_color(al_map_rgb(255, 0, 255));
-
-	al_set_target_bitmap(al_get_backbuffer(display));
-
-	event_queue = al_create_event_queue();
-	if (!event_queue) {
-		fprintf(stderr, "failed to create event_queue!\n");
-		al_destroy_bitmap(bouncer);
-		al_destroy_display(display);
-		al_destroy_timer(timer);
-		return -1;
-	}
-
-	al_register_event_source(event_queue, al_get_display_event_source(display));
-
-	al_register_event_source(event_queue, al_get_timer_event_source(timer));
-
-	al_clear_to_color(al_map_rgb(0, 0, 0));
-
+	// Atualiza a tela
 	al_flip_display();
 
-	al_start_timer(timer);
+	// Segura a execução por 10 segundos
+	al_rest(10.0);
 
-	while (1)
-	{
-		ALLEGRO_EVENT ev;
-		al_wait_for_event(event_queue, &ev);
+	// Finaliza a janela
+	al_destroy_display(janela);
 
-		if (ev.type == ALLEGRO_EVENT_TIMER) {
-			if (bouncer_x < 0 || bouncer_x > SCREEN_W - BOUNCER_SIZE) {
-				bouncer_dx = -bouncer_dx;
-			}
-
-			if (bouncer_y < 0 || bouncer_y > SCREEN_H - BOUNCER_SIZE) {
-				bouncer_dy = -bouncer_dy;
-			}
-
-			bouncer_x += bouncer_dx;
-			bouncer_y += bouncer_dy;
-
-			redraw = true;
-		}
-		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-			break;
-		}
-
-		if (redraw && al_is_event_queue_empty(event_queue)) {
-			redraw = false;
-
-			al_clear_to_color(al_map_rgb(0, 0, 0));
-
-			al_draw_bitmap(bouncer, bouncer_x, bouncer_y, 0);
-
-			al_flip_display();
-		}
-	}
-
-	al_destroy_bitmap(bouncer);
-	al_destroy_timer(timer);
-	al_destroy_display(display);
-	al_destroy_event_queue(event_queue);
+	// Libera a memória da imagem
+	al_destroy_bitmap(imagem);
 
 	return 0;
 }
