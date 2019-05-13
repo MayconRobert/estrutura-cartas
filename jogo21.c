c66a590d3d699c202f0291dc01748978d50f1077
lucasmori <lucastecnew@gmail.com>
2019-05-13 01:43:49 -0300

    gerarBaralho e embaralharBaralho criado

--- jogo21.c
+++ jogo21.c
@@ -5,3 +5,28 @@
 
 #include "jogo21.h"
 
+void gerarBaralho(CARTA baralho){
+    char naipes[4]={'o','c','e','p'};
+    int i, j, k;
+    k=0;
+        for(i=0;i<4;i++){
+            for(j=2;j<=14;j++){
+                baralho[k]->naipe=naipes[i];
+                baralho[k]->valor=j;
+                k++;                
+            }
+        }
+}
+
+void embaralharBaralho(CARTA baralho){
+    int i, j, cont;
+    CARTA aux;
+        for(cont=0;cont<200;cont++){
+            i=rand()%52; //(0-51)
+            j=rand()%52;
+            aux=baralho[i];
+            baralho[i]=baralho[j];
+            baralho[j]=aux;
+        }
+}
+
