27fb228e7ff586da75c315fb3d58d7da6eaeb064
lucasmori <lucastecnew@gmail.com>
2019-05-17 21:28:33 -0300

    Fila

--- main.c
+++ main.c
@@ -3,71 +3,100 @@
 #include <string.h>
 #include <math.h>
 
-#define MAX_CARTAS 52
+typedef struct elem{
+    int info;
+    struct elem *lig;
+}tipo_elem;
 
-const char valores = "A23456789TJQK";
+typedef struct{
+    tipo_elem *inicio;
+    tipo_elem *fim;
+}fila;
 
-typedef struct _Carta{
-        char valor;
-        char naipe;
+fila q;
 
-} CARTA;
-
-
-typedef struct _Baralho{
-    CARTA cartas[MAX_CARTAS];
-} BARALHO;
-
-
-CARTA criarCarta(char valor){
-    CARTA carta;
-    carta.valor= valor;
-
-    return carta;
+void definir(fila *q){
+/*Cria uma fila vazia. Deve ser usado antes de qualquer outra operação*/
+    q->inicio = NULL;
+    q->fim = NULL;
 }
 
-BARALHO gerarBaralho(){
-    int i = 0;
-    int j = 0;
-    BARALHO baralho;
-    for(j=0; j>=3; i++){
-        for(i=0; i<13; i++){
-            baralho.cartas[i].valor= valores[i];
+int vazia (fila *q){
+/*Retorna true se fila não contém elementos, false caso contrário*/
+    return (q->inicio == NULL);
+}
+
+void tornar_vazia (fila *q){
+/*Reinicializa uma fila existente q como uma fila vazia removendo todos os seus elementos.*/
+    tipo_elem *ndel, *nextno;
+        if(!vazia(q)){
+            nextno = q->inicio;
+                while (nextno != NULL){
+                    ndel = nextno;
+                    nextno = nextno->lig;
+                    free(ndel);
+                }
         }
+    definir(q);
+}
+
+int inserir(fila *q, int info){
+/*Adiciona um item no fim da fila q. Retorna true se operação realizada com sucesso, false caso contrário*/
+    tipo_elem *p;
+    p = malloc(sizeof(tipo_elem));
+        if (p == NULL){
+        return 0;
+        }
+            p->info = info;
+            p->lig = NULL;
+                if (vazia(q)){
+                q->inicio = p;
+
+                }else{
+                 q->fim->lig = p;
+                 q->fim = p;
+                 return 1;
+}
+
+int remover(fila *q, int *info){
+/*Remove um item do início da fila q. Retorna true se operação realizada com sucesso, false caso contrário*/
+    tipo_elem *p;
+        if(vazia(q)){
+            return 0;
+        }
+        p = q->inicio;
+        *info = p->info;
+        q->inicio = p->lig;
+            if(q->inicio == NULL){
+                q->fim = NULL;
+                }
+                free(p);
+                return 1;
+}
+
+int tamanho(fila *q){
+/*Retorna o tamanho da fila*/
+    tipo_elem *p;
+    int cont = 0;
+    p = q->inicio;
+        while(p != NULL){
+            cont ++;
+            p = p->lig;
+        }
+            return cont;
+}
+
+int comecoFila(fila *q, int *item){
+/*Mostra o começo da fila sem remover o item. Retorna true se operação realizada com sucesso, false caso contrário*/
+    if(vazia(q)){
+        return 0;
     }
-return baralho;
+        *item = q->inicio->info;
+        return 1;
 }
 
-
-/*
-BARALHO embaralharBaralho(){
-    int i, j, cont;
-    CARTA aux;
-        for(cont=0;cont<200;cont++){
-            i=rand()%52; //(0-51)
-            j=rand()%52;
-            aux=baralho[i];
-            baralho[i]=baralho[j];
-            baralho[j]=aux;
-        }
-}
-
-*/
-
-/* gerarBaralho();
-void deletarBaralho();
-void embaralharBaralho();
-
-void criarMao();
-void pegarCarta();
-void deletarMao();
-
-void divulgarResultado();
-
-*/
 
 int main(){
-    BARALHO baralho = gerarBaralho();
-printf("%c",baralho);
-
+    printf("Hello world!\n");
+    return 0;
 }
