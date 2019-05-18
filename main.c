14e6bd9af98b2292f6f3de002d41ce8e2682b502
Maria Bezerra <silvamay198@gmail.com>
2019-05-18 14:18:36 -0300

    adiciona pilha e fila

--- main.c
+++ main.c
@@ -23,6 +23,104 @@ typedef struct _baralho {
     ANIMAL animal[MAX_CARTAS];
 } BARALHO;
 
+typedef struct _celula {
+    struct _celula *prox;
+    ANIMAL carta;
+} CELULA;
+
+typedef CELULA *PTR_CELULA;
+
+//Cria pilha para baralho
+typedef struct _pilha {
+    PTR_CELULA topo;
+    int tamanho;
+} PILHA;
+
+typedef PILHA *PTR_PILHA;
+
+int esta_vazio(PTR_PILHA pilha){
+    return pilha->tamanho == 0 ? 1 : 0;
+}
+
+void inicializar(PTR_PILHA pilha){
+    pilha->tamanho = 0;
+    pilha->topo = NULL;
+}
+
+PTR_PILHA criar_pilha(){
+    PTR_PILHA pilha = (PTR_PILHA)malloc(sizeof(PTR_PILHA));
+    inicializar(pilha);
+    return pilha;
+}
+
+void empilhar(PTR_PILHA pilha, ANIMAL carta){
+    PTR_CELULA celula = (PTR_CELULA)malloc(sizeof(PTR_CELULA));
+    celula->carta = carta;
+    celula->prox = pilha->topo;
+    pilha->topo = celula;
+    pilha->tamanho++;
+}
+
+void desempilhar(PTR_PILHA pilha, ANIMAL *outCarta){
+    if (!esta_vazio(pilha)){
+        PTR_CELULA lixo = pilha->topo;
+        *outCarta = lixo->carta;
+        pilha->topo = pilha->topo->prox;
+        free(lixo);
+        pilha->tamanho--;
+    }
+}
+
+// fila para a mao do jogador
+typedef struct _fila {
+    PTR_CELULA inicio;
+    PTR_CELULA fim;
+    int tamanho;
+} FILA;
+
+typedef FILA *PTR_FILA;
+
+void inicializar_fila(PTR_FILA fila){
+    fila->inicio = NULL;
+    fila->fim = NULL;
+    fila->tamanho = 0;
+}
+
+PTR_FILA criar_fila(){
+    PTR_FILA fila = (PTR_FILA)malloc(sizeof(PTR_FILA));
+    inicializar_fila(fila);
+    return fila;
+}
+
+void inserir_fila(PTR_FILA fila, ANIMAL carta){
+    PTR_CELULA celula = (PTR_CELULA)malloc(sizeof(PTR_CELULA));
+    celula->carta = carta;
+    celula->prox = NULL;
+
+    if (fila->inicio == NULL){
+        fila->inicio = celula;
+    } else {
+        fila->fim->prox = celula;
+    }
+
+    fila->fim = celula;
+    fila->tamanho++;
+}
+
+ANIMAL remover_fila(PTR_FILA fila){
+    ANIMAL carta = fila->inicio->carta;
+
+    PTR_CELULA celulaLixo;
+    celulaLixo = fila->inicio;
+
+    fila->inicio = fila->inicio->prox;
+
+    fila->tamanho--;
+
+    free(celulaLixo);
+    return carta;
+}
+
 ANIMAL criar_animal(int id, float altura, float peso, float comprimento, float velocidade, float instintoAssasino){
     ANIMAL animal;
     animal.id = id;
