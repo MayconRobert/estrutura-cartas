8cbfd4685b6943d720f272e6c4e12fd126769ee6
Gabriel <gabriel_alonso14@hotmail.com>
2019-06-06 23:41:30 -0300

    Correcoes e melhorias

--- main.c
+++ main.c
@@ -1,351 +1,363 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
+
 #include <locale.h>
 
-typedef struct _carta
-{
-    struct _carta *proximo;
-    int valor;
-} CARTA;
+#include <time.h>
 
-typedef CARTA *PTR_CARTA;
+typedef struct _carta {
+  struct _carta * proximo;
+  int valor;
+}
+CARTA;
 
-typedef struct _baralho
-{
-    PTR_CARTA topo;
-    int tamanho;
-} BARALHO;
+typedef CARTA * PTR_CARTA;
 
-typedef BARALHO *PTR_BARALHO;
+typedef struct _baralho {
+  CARTA x;
+  struct _baralho_ * prox;
+}
+BARALHO;
 
-int esta_vazio(PTR_BARALHO baralho)
-{
-    return baralho->tamanho == 0 ? 1 : 0;
+typedef BARALHO * PTR_BARALHO;
+
+PTR_BARALHO criar_fila() {
+  PTR_BARALHO nova_celula = ((PTR_BARALHO) malloc(sizeof(BARALHO)));
+  nova_celula - > x.valor = -1;
+  nova_celula - > prox = nova_celula;
+  return nova_celula;
 }
 
-void inicializar(PTR_BARALHO baralho)
-{
-    baralho->tamanho = 0;
-    baralho->topo = NULL;
-}
-
-PTR_BARALHO criar_baralho()
-{
-    PTR_BARALHO baralho = (PTR_BARALHO)malloc(sizeof(PTR_BARALHO));
-    inicializar(baralho);
-    return baralho;
-}
-
-void empilhador(PTR_BARALHO baralho, int valor)
-{
-    PTR_CARTA carta = (PTR_CARTA)malloc(sizeof(PTR_CARTA));
-    carta->valor = valor;
-    carta->proximo = baralho->topo;
-    baralho->topo = carta;
-    baralho->tamanho++;
-}
-
-void despilhador(PTR_BARALHO baralho, int *outValor)
-{
-    if (!esta_vazio(baralho))
-    {
-        PTR_CARTA lixo = baralho->topo;
-        *outValor = lixo->valor;
-        baralho->topo = baralho->topo->proximo;
-        free(lixo);
-        baralho->tamanho--;
+void criar_baralho(PTR_BARALHO * baralho) {
+  int i, j;
+  for (i = 1; i < 14; i++) {
+    for (j = 1; j < 5; j++) {
+      PTR_BARALHO novo = ((PTR_BARALHO) malloc(sizeof(BARALHO)));
+      novo - > prox = ( * baralho) - > prox;
+      ( * baralho) - > prox = novo;
+      novo - > x.valor = i;
+      ( * baralho) - > prox = novo;
     }
+  }
 }
 
-void criar_cartas(int *pcartas)
-{
-    int i,j;
-    int x=0;
-    for(j=0; j<4; j++)
-    {
-        for(i=1; i<=13; i++)
-        {
-            if(i<10)
-            {
-                *(pcartas+x)=i;
-
-            }
-            else
-            {
-                *(pcartas+x)=10;
-            }
-            x++;
-        }
-    }
+void mostrar_baralho(PTR_BARALHO * baralho) {
+  BARALHO * aux;
+  aux = ( * baralho) - > prox;
+  while (aux - > x.valor != -1) {
+    printf(" %d |", aux - > x.valor);
+    aux = aux - > prox;
+  }
 }
 
-void embaralhar_baralho(int *pcartas)
-{
-    int i,temp,random;
-    srand(time(NULL));
-    for (i=0; i<52; i++)
-    {
-        random=rand()%52;
-        temp=*(pcartas+i);
-        *(pcartas+i)=*(pcartas+random);
-        *(pcartas+random)=temp;
-    }
+CARTA pegar_carta(PTR_BARALHO * baralho) {
+  BARALHO * aux;
+  CARTA x;
+  aux = ( * baralho) - > prox;
+  ( * baralho) - > prox = aux - > prox;
+  x = aux - > x;
+  free(aux);
+  return (x);
 }
 
-void empilhar_baralho()
-{
-    PTR_BARALHO baralho = criar_baralho();
-    int *pcartas;
-    int cartas[52];
-    pcartas=cartas;
-    criar_cartas(pcartas);
-    embaralhar_baralho(pcartas);
-    int i;
-    printf("\n\n");
-    for(i=0; i<52; i++)
-    {
-        empilhador(baralho,cartas[i]);
-        printf("%d\t",baralho->topo->valor);//esse print serve apenas como teste para ver se ta tudo funcionando
-    }
+void devolver_carta(PTR_BARALHO * baralho, CARTA carta) {
+  BARALHO * aux = ((PTR_BARALHO) malloc(sizeof(BARALHO)));
+  aux - > x = carta;
+  aux - > prox = ( * baralho) - > prox;
+  ( * baralho) - > prox = aux;
 }
 
-int pegar_carta_topo(PTR_BARALHO baralho)
-{
-    CARTA *x;
-    x = baralho->topo->valor;
-    return x;
+void embaralhar_baralho(PTR_BARALHO * baralho) {
+  srand(time(NULL));
+  BARALHO * aux, * aux0;
+  PTR_BARALHO aux1 = criar_fila(), aux2 = criar_fila();
+  CARTA x;
+  int p;
+  aux = ( * baralho) - > prox;
+  while (aux - > x.valor != -1) {
+    x = pegar_carta( & ( * baralho));
+    p = rand() % 100;
+    if (p % 2 == 0)
+      devolver_carta( & aux1, x);
+    else devolver_carta( & aux2, x);
+    aux = ( * baralho) - > prox;
+  }
+  ( * baralho) - > prox = aux1 - > prox;
+  aux = ( * baralho);
+  aux0 = ( * baralho) - > prox;
+  while (aux0 - > x.valor != -1) {
+    aux = aux - > prox;
+    aux0 = aux0 - > prox;
+  }
+  aux - > prox = aux2 - > prox;
 }
 
-int sorteio_iniciar_partida()
-{
-    srand(time(NULL));
-    int sorteio = rand() % 100;
-    if(sorteio <= 100)
-    {
-        printf("\n Quem começa é a Máquina!!");
-    }
-    else
-    {
-        printf("\n Quem começa é o Jogador!!");
-    }
-    return sorteio;
+void destruir_baralho(PTR_BARALHO * baralho) {
+  BARALHO * aux;
+  CARTA x;
+  aux = ( * baralho) - > prox;
+  while (aux - > x.valor != -1) {
+    x = pegar_carta( & ( * baralho));
+    aux = ( * baralho) - > prox;
+  }
 }
 
+typedef struct _celula {
+  struct _celula * prox;
+  PTR_CARTA carta;
+}
+CELULA;
 
-// Começo da parte de fila
-typedef struct _celula
-{
-    struct _celula *prox;
-    PTR_CARTA numero;
-} CELULA;
+typedef CELULA * PTR_CELULA;
 
-typedef CELULA *PTR_CELULA;
+//FILA MÃO DO JOGADOR
+typedef struct _fila {
+  PTR_CELULA inicio;
+  PTR_CELULA fim;
+  int tamanho;
+}
+FILA;
 
-typedef struct _fila
-{
-    PTR_CELULA inicio;
-    PTR_CELULA fim;
-    int tamanho;
-} FILA;
+typedef FILA * PTR_FILA;
 
-typedef FILA *PTR_FILA;
-
-int filaVazia(PTR_FILA fila)
-{
-    return fila->tamanho == 0 ? 1 : 0;
+void inicializar_fila(PTR_FILA fila) {
+  fila - > inicio = NULL;
+  fila - > fim = NULL;
+  fila - > tamanho = 0;
 }
 
-void inicializar_fila(PTR_FILA fila)
-{
-    fila->inicio = NULL;
-    fila->fim = NULL;
-    fila->tamanho = 0;
+PTR_FILA criar_fila_() {
+  PTR_FILA fila = (PTR_FILA) malloc(sizeof(PTR_FILA));
+  inicializar_fila(fila);
+  return fila;
 }
 
-PTR_FILA criar_fila()
-{
-    PTR_FILA fila = (PTR_FILA)malloc(sizeof(PTR_FILA));
-    inicializar_fila(fila);
-    return fila;
+void inserir_fila(PTR_FILA fila, int valor) {
+  PTR_CELULA celula_ = (PTR_CELULA) malloc(sizeof(PTR_CELULA));
+  celula_ - > carta - > valor = valor;
+  celula_ - > prox = NULL;
+
+  if (fila - > inicio == NULL) {
+    fila - > inicio = celula_;
+  } else {
+    fila - > fim - > prox = celula_;
+  }
+
+  fila - > fim = celula_;
+  fila - > tamanho++;
 }
 
-void imprimir_fila(PTR_FILA fila)
-{
-    PTR_CELULA celula = fila->inicio;
-    while (celula != NULL)
-    {
-        printf("%d ", celula->numero);
-        celula = celula->prox;
-    }
-    printf("\n");
+void imprimir_fila(PTR_FILA fila) {
+  PTR_CELULA celula_ = fila - > inicio;
+  while (celula_ != NULL) {
+    printf("%d ", celula_ - > carta - > valor);
+    celula_ = celula_ - > prox;
+  }
+  printf("\n");
 }
 
-void inserir_fila(PTR_FILA fila, PTR_CARTA numero)
-{
-    PTR_CELULA celula = (PTR_CELULA)malloc(sizeof(PTR_CELULA));
-    celula->numero = numero;
-    celula->prox = NULL;
+void jogar(PTR_BARALHO * baralho, int * placar_jogador, int * placar_computador) {
+  srand(time(NULL));
+  int soma_jogador = 0, soma_computador = 0;
+  CARTA carta_jogador, carta_computador;
+  char ch;
+  int op2;
+  PTR_FILA maojogador = criar_fila_();
+  PTR_FILA maocomputador = criar_fila_();
 
-    if(fila->inicio == NULL)
-    {
-        fila->inicio = celula;
-    }
-    else
-    {
-        fila->fim->prox = celula;
-    }
-
-    fila->fim = celula;
-    fila->tamanho++;
-}
-
-int remover_fila(PTR_FILA fila)
-{
-    int numero = fila->inicio->numero;
-
-    PTR_CELULA celulaLixo;
-    celulaLixo = fila->inicio;
-
-    fila->inicio = fila->inicio->prox;
-
-    fila->tamanho--;
-
-    free(celulaLixo);
-    return numero;
-}
-
-void jogar(PTR_BARALHO baralho){
-    int numero;
-    int soma1=0, soma2=0;
-    CARTA x1,x2;
-    int sorteio_quem_joga = sorteio_iniciar_partida();
-    do
-    {
-        x1.valor=x2.valor=0;
+  do {
+    carta_jogador.valor = carta_computador.valor = 0;
+    system("cls");
+    printf("\n----------------- MENU DO JOGO -----------------\n");
+    printf("\n1. Pegar carta \n");
+    printf("\n2. Não pegar carta \n");
+    printf("\n3. Mão do jogador\n");
+    printf("\n4. Mão do computador!\n");
+    printf("\n5. Sair do jogo \n");
+    printf("\nDigite um número correspondente a opção desejada: \n");
+    scanf("%d", & op2);
+    switch (op2) {
+    case 1:
+      {
         system("cls");
-        printf("1- Pegar Carta!\n");
-        printf("2- Passar a vez!\n");
-        printf("\nEscolha a opcao desejada: \n");
-        scanf("%d", &numero);
-
-        switch(numero)
-        {
-        case 1:
-            system("cls"); //limpar a tela
-            x1.valor = pegar_carta_topo(baralho);
-            printf("Você pegou a carta %d\n", x1.valor);
-            soma1+= x1.valor;
-            break;
-
-        case 2:
-            system("cls"); //limpar a tela
-            x2.valor = pegar_carta_topo(baralho);
-            printf("O computador pegou a carta! %d\n", x2.valor);
-            soma2+= x2.valor;
-            break;
-
-        default:
-            printf("Opcao inválida!\n ");
+        carta_jogador = pegar_carta( & ( * baralho));
+        printf("Você pegou a carta: |%d| \n\n", carta_jogador.valor);
+        inserir_fila(maojogador, carta_jogador.valor);
+        if (soma_computador == 0) {
+          carta_computador = pegar_carta( & ( * baralho));
+          printf("Computador pegou a carta: |%d| \n\n", carta_computador.valor);
+          inserir_fila(maocomputador, carta_computador.valor);
+          soma_computador += carta_computador.valor;
+        } else {
+          int r = rand() % 2;
+          if (r == 1) {
+            carta_computador = pegar_carta( & ( * baralho));
+            soma_computador += carta_computador.valor;
+            printf("Computador pegou a carta |%d| \n\n", carta_computador.valor);
+            inserir_fila(maocomputador, carta_computador.valor);
+          } else printf("Computador não pegou nenhuma carta! \n\n");
         }
+        soma_jogador += carta_jogador.valor;
+        scanf("%c", & ch);
+        break;
+      }
+    case 2:
+      {
+        system("cls");
+        printf("Você não pegou nenhuma carta! \n\n");
+        if (soma_computador == 0) {
+          carta_computador = pegar_carta( & ( * baralho));
+          printf("Computador pegou a carta |%d| \n\n", carta_computador.valor);
+          inserir_fila(maocomputador, carta_computador.valor);
+          soma_computador += carta_computador.valor;
+        } else {
+          int r = rand() % 2;
+          if (r == 1) {
+            carta_computador = pegar_carta( & ( * baralho));
+            soma_computador += carta_computador.valor;
+            printf("Computador pegou a carta |%d| \n\n", carta_computador.valor);
+            inserir_fila(maocomputador, carta_computador.valor);
+          } else printf("Computador não pegou nenhuma carta! \n\n");
+        }
+        soma_jogador += carta_jogador.valor;
+        scanf("%c", & ch);
+        break;
+      }
+    case 3:
+      {
+        system("cls");
+        printf("\nMão do jogador:");
+        imprimir_fila(maojogador);
+        scanf("%c", & ch);
+        break;
+      }
+    case 4:
+      {
+        system("cls");
+        printf("\nMão do computador:");
+        imprimir_fila(maocomputador);
+        scanf("%c", & ch);
+        break;
+      }
+    case 5:
+      {
+        system("cls");
+        printf("\nSaindo do Jogo\n");
+        scanf("%c", & ch);
+        break;
+      }
+    default:
+      {
+        printf("\nValor incorreto!!\n\n");
+        scanf("%c", & ch);
+      }
     }
-    while((soma1 < 21) && (soma2 < 21));
-
-    if ((soma1>21) && (soma2>21))
-    {
-        printf("O jogo ficou empatado\n");
-    }
-    else if(soma1 > 21)
-    {
-        printf("Você ganhou esta rodada\n");
-    }
-    else if(soma2 > 21)
-    {
-        printf("Você perdeu esta rodada\n");
-    }
-    else if(soma1 == 21)
-    {
-        printf("Você ganhou esta rodada\n");
-    }
-    else if(soma2 == 21)
-    {
-        printf("Você perdeu esta rodada\n");
-    }
-    else if ((soma1==21) && (soma2==21))
-    {
-        printf("O jogo ficou empatado\n");
-    }
-    else if (soma1>soma2)
-    {
-        printf("Você ganhou esta rodada\n");
-    }
-    else if (soma1<soma2)
-    {
-        printf("Você perdeu esta rodada\n");
-    }
-    else
-    {
-        printf("O jogo ficou empatado\n");
-    }
+    printf("--------- PONTUAÇÃO DO JOGO ----------\n\n");
+    printf("Soma do Jogador = %d\n\n", soma_jogador);
+    printf("Soma do Computador = %d\n\n", soma_computador);
+    scanf("%c", & ch);
+  } while ((soma_jogador < 21) && (soma_computador < 21) && (op2 != 3));
+  if ((soma_jogador > 21) && (soma_computador > 21))
+    printf("O jogo ficou empatado\n");
+  else if (soma_jogador > 21) {
+    printf("Você perdeu esta rodada\n");
+    ( * placar_computador) ++;
+  } else if (soma_computador > 21) {
+    printf("Você ganhou esta rodada\n");
+    ( * placar_jogador) ++;
+  } else if (soma_jogador == 21) {
+    printf("Você ganhou esta rodada\n");
+    ( * placar_jogador) ++;
+  } else if (soma_computador == 21) {
+    printf("Você perdeu esta rodada\n");
+    ( * placar_computador) ++;
+  } else if ((soma_jogador == 21) && (soma_computador == 21))
+    printf("O jogo ficou empatado\n");
+  else if (soma_jogador > soma_computador) {
+    printf("Você ganhou esta rodada\n");
+    ( * placar_jogador) ++;
+  } else if (soma_jogador < soma_computador) {
+    printf("Você perdeu esta rodada\n");
+    ( * placar_computador) ++;
+  } else printf("O jogo ficou empatado\n");
+  destruir_baralho( & ( * baralho));
 }
-
-int main()
-{
-setlocale(LC_ALL, "Portuguese");
-
-//PTR_FILA fila = criar_fila();
-//empilhar_baralho();
-//inserir_fila(fila, 3);
-//inserir_fila(fila, 5);
-//inserir_fila(fila, 7);
-//remover_fila(fila);
-//imprimir_fila(fila);
-
-//Testar o jogo
-    //Primeiro: precisamos dessas funções acionadas:
-         /*Criar a Fila*/
-        /*Criar o Baralho*/
-        /*Embaralhar o Baralho*/
-
-    //Segundo: obter um menu para testar o jogo
-int op;
-do{
-     system("cls");
-     printf("\n*************** Menu ***************\n\n");
-     printf("\n1- Mostrar o Baralho\n");
-     printf("\n2- Jogar\n");
-     printf("\n3- Sair \n");
-     printf("\nEntre com a sua Opção: \n");
-     scanf("%d", &op);
-    switch(op){
-        case 1:{
-           system("cls");
-           printf("Mostrando o Baralho\n");
-           empilhar_baralho();
-           break;
-        }
-        case 2:{
-           system("cls");
-           /*Criando o Baralho */
-            PTR_BARALHO baralho = criar_baralho();
-           jogar(&baralho);
-           /*Embaralhando o Baralho*/
-           int *pcartas;
-           int cartas[52];
-           pcartas=cartas;
-           embaralhar_baralho(pcartas);
-           break;
-        }
-         case 3:{
-           system("cls");
-           printf("\nSaindo do Programa\n");
-           exit (0);
-           break;
-        }
-        default:{
-           printf("\nEntre com um valor válido!\n");
-        }
+int main(void) {
+  setlocale(LC_ALL, "Portuguese");
+  char ch;
+  int op;
+  system("cls");
+  PTR_BARALHO baralho = criar_fila();
+  criar_baralho( & baralho);
+  int i;
+  for (i = 0; i < 10; i++)
+    embaralhar_baralho( & baralho);
+  int placar_jogador = 0, placar_computador = 0;
+  do {
+    system("cls");
+    printf("\n----------------- MENU DO JOGO -----------------\n\n");
+    printf("\n1.Olhar o Baralho\n");
+    printf("\n2.Jogar\n");
+    printf("\n3.Olhar a Pontuação do jogo\n");
+    printf("\n4.Zerar a Pontuação do jogo\n");
+    printf("\n5.Sair do jogo \n");
+    printf("\nDigite o número da opção desejada: \n");
+    scanf("%d", & op);
+    switch (op) {
+    case 1:
+      {
+        system("cls");
+        printf("Mostrando o Baralho\n\n");
+        mostrar_baralho( & baralho);
+        scanf("%c", & ch);
+        break;
+      }
+    case 2:
+      {
+        system("cls");
+        jogar( & baralho, & placar_jogador, & placar_computador);
+        scanf("%c", & ch);
+        criar_baralho( & baralho);
+        int i;
+        for (i = 0; i < 10; i++)
+          embaralhar_baralho( & baralho);
+        break;
+      }
+    case 3:
+      {
+        system("cls");
+        printf("Placar de %s : %d \n", "Jogador: ", placar_jogador);
+        printf("Placar do %s : %d \n", "Computador: ", placar_computador);
+        scanf("%c", & ch);
+        break;
+      }
+    case 4:
+      {
+        system("cls");
+        placar_jogador = placar_computador = 0;
+        printf("Placar Zerado\n");
+        scanf("%c", & ch);
+        break;
+      }
+    case 5:
+      {
+        system("cls");
+        printf("\nSaindo do Programa\n");
+        scanf("%c", & ch);
+        exit(0);
+        break;
+      }
+    default:
+      {
+        printf("\nValor incorreto!\n");
+        scanf("%c", & ch);
+      }
     }
-}while (op != 3);
-return 0;
+    scanf("%c", & ch);
+  } while (op != 5);
+
+  return 0;
 }
